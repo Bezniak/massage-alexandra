2871575f2989b0a32d9e4b7e71e5d7d5f4777cc9
ivanbezniak <ivan.bezniak@gmail.com>
2025-03-09 15:29:14 +0300

    12 commit fixing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,70 +108,82 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="px-4 py-6 bg-gray-900 md:flex md:items-center md:justify-between">
-                    <span className="text-base text-white sm:text-center">
-                        © {year} <NavLink to={ROUTES.HOME} onClick={handleClick}>Massage Grodno™</NavLink>.
-                        &nbsp;
-                        <a href="https://contragent.by/unp/590210932"
-                           target="_blank"
-                           rel="noopener noreferrer"
-                           className="hover:text-white transition"
-                        >
-                            {t("UNP")}
-                        </a>
-                        &nbsp;
-                        {t("all_right_reserved")}
-                    </span>
-
-                    <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-                        <a href="https://www.facebook.com/profile.php?id=100070173889307" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor" viewBox="0 0 8 19">
-                                <path fillRule="evenodd"
-                                      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                                      clipRule="evenodd"/>
-                            </svg>
-                            <span className="sr-only">Facebook page</span>
-                        </a>
-                        <a href="https://www.instagram.com/ivsoft.by" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <FaInstagramSquare className="w-6 h-6"/>
-                        </a>
-                        <a
-                            href="https://www.tiktok.com/@ivsoft.by"
-                            target='_blank'
-                            className="text-gray-400 hover:text-white"
-                        >
-                            <FaTiktok className="w-6 h-6"/>
-                        </a>
-                        <a href="viber://chat?number=%2B375295210417"
-                           target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <FaViber className="w-6 h-6"/>
-                        </a>
-                        <a href="https://github.com/Bezniak"
-                           target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd"
-                                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                                      clipRule="evenodd"/>
-                            </svg>
-                            <span className="sr-only">GitHub account</span>
-                        </a>
-                        <a href="https://t.me/ivan_bezniak" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <FaTelegram className="w-6 h-6"/>
-                        </a>
-                        <a href="mailto:ivsoft.ask@gmail.com" target='_blank'
-                           className="text-gray-400 hover:text-white">
-                            <MdOutlineMailOutline className="w-6 h-6"/>
-                        </a>
+                <div className="px-4 py-6 bg-gray-900 flex flex-col items-center justify-between">
+                    <div className="flex mt-4 justify-between w-full space-x-5 rtl:space-x-reverse">
+                        <div>
+                                <span className="text-base text-white sm:text-center">
+                                © {year} <NavLink to={ROUTES.HOME} onClick={handleClick}>Massage Grodno™</NavLink>.
+                                    &nbsp;
+                                    <a href="https://contragent.by/unp/590210932"
+                                       target="_blank"
+                                       rel="noopener noreferrer"
+                                       className="hover:text-white transition"
+                                    >
+                                        {t("UNP")}
+                                    </a>
+                                    &nbsp;
+                                    {t("all_right_reserved")}
+                            </span>
+                        </div>
+                        <div className='flex gap-8'>
+                            <a href="https://www.facebook.com/profile.php?id=100070173889307" target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                     fill="currentColor" viewBox="0 0 8 19">
+                                    <path fillRule="evenodd"
+                                          d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                                <span className="sr-only">Facebook page</span>
+                            </a>
+                            <a href="https://www.instagram.com/ivsoft.by" target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <FaInstagramSquare className="w-6 h-6"/>
+                            </a>
+                            <a
+                                href="https://www.tiktok.com/@ivsoft.by"
+                                target='_blank'
+                                className="text-gray-400 hover:text-white"
+                            >
+                                <FaTiktok className="w-6 h-6"/>
+                            </a>
+                            <a href="viber://chat?number=%2B375295210417"
+                               target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <FaViber className="w-6 h-6"/>
+                            </a>
+                            <a href="https://github.com/Bezniak"
+                               target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                     fill="currentColor" viewBox="0 0 20 20">
+                                    <path fillRule="evenodd"
+                                          d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                                <span className="sr-only">GitHub account</span>
+                            </a>
+                            <a href="https://t.me/ivan_bezniak" target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <FaTelegram className="w-6 h-6"/>
+                            </a>
+                            <a href="mailto:ivsoft.ask@gmail.com" target='_blank'
+                               className="text-gray-400 hover:text-white">
+                                <MdOutlineMailOutline className="w-6 h-6"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='text-center mt-5'>
+                        <p className='text-white text-sm'>
+                            {t("developed_by")} &nbsp;
+                            <a href="https://www.linkedin.com/in/ivan-bezniak-2634a11a0/"
+                               rel="noreferrer"
+                               target="_blank"
+                               className='hover:text-[var(--white)] transition'
+                            > {t("ivan_bezniak")}</a>
+                        </p>
                     </div>
                 </div>
-
             </div>
         </footer>
 
